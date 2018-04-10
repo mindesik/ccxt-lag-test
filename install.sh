603b90e9c66fab7cf3c25800da1edd3717b2c1a5
민 제식 <ds@codesmith.ru>
2018-04-10 19:39:45 +1100

    Add install script

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ND_VERSION=8.11.1
+wget https://nodejs.org/dist/v$ND_VERSION/node-v$ND_VERSION-linux-x64.tar.gz && tar -C /usr/local --strip-components 1 -xzf node-v$ND_VERSION-linux-x64.tar.gz && rm node-v$ND_VERSION-linux-x64.tar.gz
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
+apt update
+apt install --no-install-recommends yarn git -y
+
+git clone git@github.com:misterpaladin/ccxt-lag-test.git

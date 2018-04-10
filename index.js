ed6bc0c7c690f122ca8548f43c37907a9f1c88f2
민 제식 <ds@codesmith.ru>
2018-04-10 20:27:56 +1100

    Tests, write to database

--- index.js
+++ index.js
@@ -1,5 +1,75 @@
 require('dotenv').config()
+const ccxt = require('ccxt')
+const moment = require('moment')
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  dialect: 'mysql',
+  logging: typeof process.env.DEBUG !== 'undefined' && process.env.DEBUG == 'true',
+})
 
-setInterval(() => {
-  console.log(process.env)
-}, 1000)
+const Log = sequelize.define('lagLoggers', {
+  env: {
+    type: Sequelize.STRING,
+  },
+  exchange: {
+    type: Sequelize.STRING,
+  },
+  result: {
+    type: Sequelize.STRING,
+  },
+  lag: {
+    type: Sequelize.INTEGER,
+  },
+  errorMessage: {
+    type: Sequelize.TEXT,
+  },
+})
+
+let start = async () => {
+  console.log('Starting..')
+  let exchanges = {
+    BNNC: new ccxt.binance(),
+    BTRX: new ccxt.bittrex(),
+    KUCN: new ccxt.kucoin(),
+    YOBT: new ccxt.yobit(),
+    PLNX: new ccxt.poloniex(),
+    BLTR: new ccxt.bleutrade(),
+    LVCN: new ccxt.livecoin(),
+    LQUI: new ccxt.liqui(),
+    CNXC: new ccxt.coinexchange(),
+    GATE: new ccxt.gateio(),
+    BITF: new ccxt.bitfinex2(),
+    EXMO: new ccxt.exmo(),
+    CBNH: new ccxt.cobinhood(),
+    CEXI: new ccxt.cex(),
+    CRTP: new ccxt.cryptopia(),
+    GDAX: new ccxt.gdax(),
+    GMNI: new ccxt.gemini(),
+    HITB: new ccxt.hitbtc2(),
+    HUBI: new ccxt.huobipro(),
+    BTBY: new ccxt.bitbay(),
+  }
+
+  for (let i in Object.keys(exchanges)) {
+    let startedAt = moment()
+    let code = Object.keys(exchanges)[i]
+    let exchange = exchanges[code]
+    await exchange.fetchTicker('LTC/BTC')
+    await Log.create({
+      env: process.env.ENV,
+      exchange: code,
+      lag: moment().diff(startedAt),
+    })
+
+    if (parseInt(i) + 1 === Object.keys(exchanges).length) {
+      setTimeout(start, 10000)
+    }
+  }
+}
+
+start()
